59bf01b30439d3c28d2ac977899b227ab0a662da
freeliu <liurc@outlook.com>
2023-06-15 14:18:01 +0800

    github workflow test 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,13 @@ require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
   root: true,
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/eslint-config-typescript", "@vue/eslint-config-prettier"],
-  env: {
-    node: true,
-    browser: true
-  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting"
+  ],
   parserOptions: {
     ecmaVersion: "latest"
-  },
-  rules: {
-    "prettier/prettier": [1, { semi: false, trailingComma: "none", printWidth: 160 }]
   }
 }
